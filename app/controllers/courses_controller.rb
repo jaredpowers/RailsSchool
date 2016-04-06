892caedfd140190bbe13edec3c05083a0d523a38
JaredPowers <jarpow44@gmail.com>
2016-04-06 01:59:17 -0500

    input controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,11 @@
 class CoursesController < ApplicationController
+
+  def index
+    page = (params[:page] || 0).to_i
+    courses = Course.limit(25).offset(page * 25)
+    render locals: { courses: courses, page: page }
+  end
+
+  def show
+  end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,14 @@
 class TeachersController < ApplicationController
+
+  def index
+    page  = (params[:page]  ||  0).to_i
+    teachers = Teacher.limit(25).offset(page * 25)
+    render locals: { teachers: teachers, page: page }
+  end
+
+  def show
+    teacher = Teacher.find(params[:id])
+    render locals: { teacher: teacher }
+  end
+  
 end
